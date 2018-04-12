5e338b3ff4fbac8c992fa5cd8ff355b824c50ee3
Satit Rianpit <siteslave@msn.com>
2018-04-12 11:51:37 +0700

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localeTh from '@angular/common/locales/th';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+
+library.add(fas, far);
+
+registerLocaleData(localeTh, 'th');
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    NgbModule.forRoot(),
+    FontAwesomeModule
   ],
-  providers: [],
+  providers: [{ provide: LOCALE_ID, useValue: 'th-TH' }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
